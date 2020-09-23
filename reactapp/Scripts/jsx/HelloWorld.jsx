00681694af3e85b2b6df27dfce0441b96bfd8087
povilasurbonas <povilas.urbonas85@gmail.com>
2020-09-23 07:42:17 +0200

    Added git ignore file

---
+++ reactapp/Scripts/jsx/HelloWorld.jsx
@@ -0,0 +1,11 @@
+﻿class HelloWorld extends React.Component {
+    render() {
+        return (<div>
+            <h1>Hello World!!  !</h1>
+        </div>);
+    }
+}
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('content')
+);  
